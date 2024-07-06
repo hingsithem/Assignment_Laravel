2fead564464af440f4efdd7b18022b3441a1a3c7
hingsithem <sithemhing@gmail.com>
2024-07-06 14:14:30 +0700

    my files

---
+++ 16-06-2024/script.js
@@ -0,0 +1,9 @@
+
+function toggleMenu() {
+    const navbarLinks = document.getElementById("navbar-links");
+    if (navbarLinks.style.display === "flex") {
+        navbarLinks.style.display = "none";
+    } else {
+        navbarLinks.style.display = "flex";
+    }
+}
